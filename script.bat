79f5625f86d8d014701c2c705be2a04f7f567b44
Om-Ghadage <ghadageom22@gmail.com>
2026-04-15 17:43:41 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,13 @@
+@echo off
+echo Deploying application...
 
+REM Delete old ROOT folder
+rmdir /S /Q C:\apache-tomcat\webapps\ROOT 2>nul
+
+REM Create new ROOT folder
+mkdir C:\apache-tomcat\webapps\ROOT
+
+REM Copy files from Jenkins workspace
+xcopy /E /I /Y "%WORKSPACE%\*" C:\apache-tomcat\webapps\ROOT\
+
+echo Deployment completed!
